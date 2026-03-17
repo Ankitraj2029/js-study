c8998b30bb7257a6378b51b9faa2fa6043f35bb8
ANKIT RAJ <ar9146707@gmail.com>
2026-03-17 04:43:02 +0000

    container config done

---
+++ 01_basic/basic.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountemail = "ankit@its.com"
+var accountPassword = "12345"
+accountCity = "bihar"
+
+//  accountId = 2 
+accountEmail = "ar@gamil.com"
+accountPassword = "2323234554"
+accountCity = "saharsha"
+
+console.log(accountId);
+
+/*
+prefer not use var becouse
+of issue in block scope and
+functional scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
